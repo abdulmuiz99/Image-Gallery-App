3435088ec9c723d19ed59ba78f9d05c200b13428
Moiz <abdulmuizsiddique99@gmail.com>
2022-05-03 13:07:27 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import 'bootswatch/dist/materia/bootstrap.min.css';
+import Search from './Search';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
